a282f125466bb90d4edd1575dad8b5129763014b
Raul Garcia <rraulg5@gmail.com>
2021-10-17 22:40:48 -0500

    Navbar Responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,109 +1,122 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+import { mobile } from '../responsive';
+import { MENU_ITEMS } from '../utils/constants';
 import {
+  faBars,
   faSearch,
   faShoppingCart,
-  faUserCircle,
 } from '@fortawesome/free-solid-svg-icons';
-import styled from 'styled-components';
-import { MENU_ITEMS } from '../utils/constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Container>
-      <Wrapper>
-        <Left>
-          <Logo>FURNITFY</Logo>
-        </Left>
-        <Center>
-          <MenuContainer>
-            {MENU_ITEMS.map((menu) => (
-              <MenuItem key={menu.title}>
-                <MenuItemLink href={menu.url}>{menu.title}</MenuItemLink>
-              </MenuItem>
-            ))}
-          </MenuContainer>
-        </Center>
-        <Right>
-          <SearchContainer>
-            <Input placeholder="Search..." />
-            <SearchIcon icon={faSearch} />
-          </SearchContainer>
-          <FontAwesomeIcon
-            icon={faShoppingCart}
-            style={{ color: '#222', fontSize: 25, marginLeft: 30 }}
-          />
-          <FontAwesomeIcon
-            icon={faUserCircle}
-            style={{ color: '#666', fontSize: 35, marginLeft: 30 }}
-          />
-        </Right>
-      </Wrapper>
-    </Container>
+    <Nav>
+      <Logo href="/#">
+        FURNIT<span>FY</span>
+      </Logo>
+      <MobileMenuIcon icon={faBars} onClick={() => setIsOpen(!isOpen)} />
+      <Menu isOpen={isOpen}>
+        {MENU_ITEMS.map((menu) => (
+          <MenuLink href={menu.url} key={menu.title}>
+            {menu.title}
+          </MenuLink>
+        ))}
+        <MenuLink href="/#">
+          <CartIcon icon={faShoppingCart} />
+        </MenuLink>
+      </Menu>
+      <SearchContainer isOpen={isOpen}>
+        <Input placeholder="Search..." />
+        <SearchIcon icon={faSearch} />
+      </SearchContainer>
+    </Nav>
   );
 };
 
-const Container = styled.div`
+const Nav = styled.nav`
+  align-items: center;
   background-color: #eee;
-  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 0 1.5rem;
 `;
 
-const Wrapper = styled.div`
+const Logo = styled.a`
+  font-size: 2rem;
+  font-weight: bold;
+  color: #222;
+  padding: 1rem 0;
+  text-decoration: none;
+
+  span {
+    font-weight: lighter;
+  }
+
+  ${mobile({ fontSize: 24 })}
+`;
+
+interface MenuProps {
+  isOpen: boolean;
+}
+
+const Menu = styled.div<MenuProps>`
+  align-items: center;
   display: flex;
   justify-content: space-between;
-  padding: 15px 30px;
-`;
+  position: relative;
 
-const Left = styled.div`
-  align-items: center;
-  display: flex;
-  flex: 1;
+  ${mobile({
+    flexDirection: 'column',
+    overflow: 'hidden',
+    width: '100%',
+    maxHeight: ({ isOpen }: MenuProps) => (isOpen ? '300px' : 0),
+    transition: 'max-height 0.5s ease-in',
+  })}
 `;
-const Center = styled.div`
-  align-items: center;
-  display: flex;
-  flex: 2;
-  justify-content: center;
-`;
-const Right = styled.div`
-  align-items: center;
-  display: flex;
-  flex: 1;
-  justify-content: flex-end;
-`;
-
-export const Logo = styled.h1`
-  font-size: 40px;
-`;
-
-const MenuContainer = styled.ul`
-  align-items: center;
-  display: flex;
-  list-style-type: none;
-`;
-
-const MenuItem = styled.li`
-  cursor: pointer;
-  font-size: 20px;
-  font-weight: bold;
-  margin: 0 25px;
-`;
-
-const MenuItemLink = styled.a`
+const MenuLink = styled.a`
   color: #666;
+  font-size: 1.1rem;
+  font-weight: bold;
+  padding: 1rem 2rem;
+  text-align: center;
   transition: all 0.5s ease;
   text-decoration: none;
+  text-transform: capitalize;
 
   &:hover {
     color: #222;
   }
 `;
 
-const SearchContainer = styled.div`
+const MobileMenuIcon = styled(FontAwesomeIcon)`
+  color: '#222';
+  cursor: pointer;
+  display: none;
+  font-size: 1.5rem;
+
+  ${mobile({ display: 'flex' })}
+`;
+
+const SearchContainer = styled.div<MenuProps>`
   align-items: center;
   display: flex;
-  display: flex;
-  margin-right: 25px;
-  padding: 5px;
+  justify-content: space-between;
+  position: relative;
+  margin-right: 30px;
+
+  ${mobile({
+    flexDirection: 'row',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    width: '100%',
+    maxHeight: ({ isOpen }: MenuProps) => (isOpen ? '300px' : 0),
+    marginBottom: '0.5rem',
+    transition: 'max-height 0.5s ease-in',
+  })}
 `;
 
 const Input = styled.input`
@@ -112,8 +125,14 @@ const Input = styled.input`
   padding: 7px 25px;
 `;
 
+const CartIcon = styled(FontAwesomeIcon)`
+  color: #222;
+  font-size: 1.5rem;
+  ${mobile({ display: 'flex' })};
+`;
+
 const SearchIcon = styled(FontAwesomeIcon)`
   color: #777;
-  font-size: 16px;
-  margin-left: -30px;
+  font-size: 1rem;
+  margin-left: -2.5rem;
 `;
