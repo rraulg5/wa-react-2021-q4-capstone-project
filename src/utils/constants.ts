96096ec39fac948aab608626e960e79c106b5db7
Raul Garcia <rraulg5@gmail.com>
2021-10-26 18:19:46 -0500

    Fixing constants file conflict

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
 export const FOOTER_TEXT =
   'Ecommerce created during Wizeline’s Academy React Bootcamp';
 
+export const API_BASE_URL = 'https://wizeline-academy.cdn.prismic.io/api/v2';
+
 export const MENU_ITEMS = [
   { title: 'Products', url: '#' },
   { title: 'Rooms', url: '#' },
