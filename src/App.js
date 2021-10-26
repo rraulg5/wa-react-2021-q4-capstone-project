1be3c5b985a8887aab0eb535565c1da297706d60
JP Arc <jp.arciga@gmail.com>
2021-10-26 08:33:39 -0500

    Add sample hooks to consume API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
 
 function App() {
+  const { data, isLoading } = useFeaturedBanners();
+  console.log(data, isLoading);
+
   return (
     <div className="App">
       <header className="App-header">
