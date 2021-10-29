694d912f987a635d9ce1094c0e42e212ede662b1
Raul Garcia <rraulg5@gmail.com>
2021-10-29 09:18:27 -0500

    Product List page updated

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,31 +1,103 @@
-import { SyntheticEvent, useState } from 'react';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import { faSpinner, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 
 import { ProductList } from '../components/ProductList';
-import { Result as ResultCategory } from '../interfaces/ProductCategoriesResponse';
+import {
+  ProductCategoriesState,
+  Result as ResultCategory,
+} from '../interfaces/ProductCategoriesResponse';
 import { Result as ResultProduct } from '../interfaces/ProductsResponse';
 import { mobile } from '../responsive';
+import { useCategories } from '../utils/hooks/useCategories';
+import { API_BASE_URL } from '../utils/constants';
+import { useLatestAPI } from '../utils/hooks/useLatestAPI';
+import { ProductsResponse } from '../interfaces/ProductsResponse';
+import { useLocation } from 'react-router';
 
 export const Products = () => {
-  const categories: ResultCategory[] = [];
-  const productsMock: ResultProduct[] = [];
-  const paginationMock = Array.from(Array(5).keys());
+  const { ref: apiRef, isLoading: isLoadingApiRef } = useLatestAPI();
+
+  const query = new URLSearchParams(useLocation().search);
+  const [categoryURL] = useState(query.get('category'));
+  // const history = useHistory();
+  const [pagination, setPagination] = useState<number[]>([]);
+  // const [page, setPage] = useState(Number(query.get('page')) || 1);
+  const [page, setPage] = useState(1);
 
   const [filters, setFilters] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
 
-  const handleClick = (e: SyntheticEvent<HTMLSpanElement>) => {
-    const category = e.currentTarget.textContent?.toLowerCase()!;
+  const {
+    data: dataCategories,
+    isLoading: isLoadingCategories,
+  }: ProductCategoriesState = useCategories();
+  const [categories, setCategories] = useState<ResultCategory[]>([]);
 
+  const [isLoadingProducts, setIsLoadingProducts] = useState(true);
+  const [products, setProducts] = useState<ResultProduct[]>([]);
+
+  useEffect(() => {
+    if (!isLoadingCategories) {
+      setCategories(dataCategories.results);
+    }
+  }, [dataCategories, isLoadingCategories]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function getProducts() {
+      try {
+        const response = await fetch(
+          `${API_BASE_URL}/documents/search?ref=${apiRef}&q=${encodeURIComponent(
+            '[[at(document.type, "product")]]'
+          )}&lang=en-us&pageSize=12&page=${page}`,
+          {
+            signal: controller.signal,
+          }
+        );
+        const data: ProductsResponse = await response.json();
+
+        setProducts(data.results);
+        setPagination(Array.from(Array(data.total_pages + 1).keys()));
+        setIsLoadingProducts(false);
+      } catch (err) {
+        setProducts([]);
+        console.error(err);
+      }
+    }
+
+    if (!isLoadingApiRef) {
+      getProducts();
+    }
+
+    return () => {
+      controller.abort();
+    };
+  }, [apiRef, isLoadingApiRef, page]);
+
+  useEffect(() => {
+    if (categoryURL) {
+      setFilters([categoryURL]);
+    }
+  }, [categoryURL, categories]);
+
+  const toogleFilter = (category: string) => {
     if (!filters.includes(category)) {
       setFilters([...filters, category]);
-      e.currentTarget.style.backgroundColor = '#eee';
     } else {
-      const auxArr = filters.filter((c) => c !== category);
-      setFilters(auxArr);
-      e.currentTarget.style.backgroundColor = '#fff';
+      setFilters(filters.filter((c) => c !== category));
+    }
+  };
+
+  const clearFilters = () => {
+    setFilters([]);
+  };
+
+  const handleClickPage = (page: number) => {
+    if (page > 0 && page <= pagination.length - 1) {
+      setPage(page);
+      // history.push(`?page=${page}`);
     }
   };
 
@@ -33,16 +105,39 @@ export const Products = () => {
     <Container>
       <Sidebar>
         <h2>Categories</h2>
-        <CategoriesWrapper>
-          {categories.map((category) => (
-            <Category onClick={handleClick} key={category.id}>
-              {category.data.name}
-            </Category>
-          ))}
-        </CategoriesWrapper>
+        {isLoadingCategories ? (
+          <Loading>
+            <FontAwesomeIcon
+              icon={faSpinner}
+              spin={true}
+              style={{ fontSize: '2em', opacity: 0.7 }}
+            />
+            <br />
+            Loading...
+          </Loading>
+        ) : (
+          <CategoriesWrapper>
+            {categories.map((category) => (
+              <Category
+                onClick={() => {
+                  toogleFilter(category.slugs[0]);
+                }}
+                key={category.id}
+                className={filters.includes(category.slugs[0]) ? 'active' : ''}
+              >
+                {category.data.name}
+              </Category>
+            ))}
+          </CategoriesWrapper>
+        )}
+        {filters.length > 0 && (
+          <ClearFilters onClick={clearFilters}>
+            Clear filters <FontAwesomeIcon icon={faTimesCircle} />
+          </ClearFilters>
+        )}
       </Sidebar>
       <Main>
-        {isLoading ? (
+        {isLoadingProducts ? (
           <Loading>
             <FontAwesomeIcon
               icon={faSpinner}
@@ -56,10 +151,10 @@ export const Products = () => {
           <>
             <ProductListWrapper>
               {filters.length === 0 ? (
-                <ProductList products={productsMock} />
+                <ProductList products={products} />
               ) : (
                 <ProductList
-                  products={productsMock.filter((product) =>
+                  products={products.filter((product) =>
                     filters.includes(product.data.category.slug)
                   )}
                 />
@@ -67,16 +162,36 @@ export const Products = () => {
             </ProductListWrapper>
             <PaginationWrapper>
               <Pagination>
-                <PaginationItem className="pag-nav">Prev</PaginationItem>
-                {paginationMock.map((page) => (
-                  <PaginationItem
-                    className={page === 0 ? 'current' : ''}
-                    key={page}
-                  >
-                    {page + 1}
-                  </PaginationItem>
-                ))}
-                <PaginationItem className="pag-nav">Next</PaginationItem>
+                <PaginationItem
+                  className="pag-nav"
+                  onClick={() => {
+                    handleClickPage(page - 1);
+                  }}
+                >
+                  Prev
+                </PaginationItem>
+                {pagination.map(
+                  (pag) =>
+                    pag !== 0 && (
+                      <PaginationItem
+                        className={pag === page ? 'current' : ''}
+                        key={pag}
+                        onClick={() => {
+                          handleClickPage(pag);
+                        }}
+                      >
+                        {pag}
+                      </PaginationItem>
+                    )
+                )}
+                <PaginationItem
+                  className="pag-nav"
+                  onClick={() => {
+                    handleClickPage(page + 1);
+                  }}
+                >
+                  Next
+                </PaginationItem>
               </Pagination>
             </PaginationWrapper>
           </>
@@ -119,6 +234,15 @@ const Category = styled.span`
   font-size: 1.2em;
   margin: 0.45em;
   padding: 0.5em;
+
+  &.active {
+    background-color: #eee;
+  }
+`;
+
+const ClearFilters = styled.span`
+  cursor: pointer;
+  text-decoration: underline;
 `;
 
 const Sidebar = styled.div`
