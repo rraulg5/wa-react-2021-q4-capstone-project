85c8f6b134881de034f17baf02da561fe44b53e7
Raul Garcia <rraulg5@gmail.com>
2021-11-12 13:36:43 -0600

    React Query installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import routes from './config/routes';
 import RouteItem from './interfaces/RouteItem';
 
@@ -6,26 +7,30 @@ import { Layout } from './components/Layout';
 import { CartProvider } from './context/CartProvider';
 import ErrorBoundary from './components/ErrorBoundary';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <ErrorBoundary>
-      <CartProvider>
-        <Router basename={process.env.PUBLIC_URL}>
-          <Layout>
-            <Switch>
-              {routes.map((r: RouteItem) => (
-                <Route
-                  key={r.key}
-                  path={r.path}
-                  component={r.component}
-                  exact={r.exact}
-                />
-              ))}
-            </Switch>
-          </Layout>
-        </Router>
-      </CartProvider>
-    </ErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        <CartProvider>
+          <Router basename={process.env.PUBLIC_URL}>
+            <Layout>
+              <Switch>
+                {routes.map((r: RouteItem) => (
+                  <Route
+                    key={r.key}
+                    path={r.path}
+                    component={r.component}
+                    exact={r.exact}
+                  />
+                ))}
+              </Switch>
+            </Layout>
+          </Router>
+        </CartProvider>
+      </ErrorBoundary>
+    </QueryClientProvider>
   );
 };
 
