231bdaa3f79dec18a3b19c19180206089ba1a839
Raul Garcia <rraulg5@gmail.com>
2021-11-16 09:46:12 -0600

    Replace of useLatestAPI due to errors in tests

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,7 +1,34 @@
 import { useQuery } from 'react-query';
 import { API_BASE_URL } from '../utils/constants';
-import { useLatestAPI } from '../utils/hooks/useLatestAPI';
 import { APIResponse } from '../interfaces/APIResponse';
+import { APIRefResponse } from '../interfaces/APIRefResponse';
+
+export const useFetch = (fetchKey: string, endpoint: string) => {
+  let apiRef = '';
+  const { data: apiData } = useQuery<APIRefResponse, Error>(
+    'apiRef',
+    fetchAPIRef
+  );
+  if (apiData) {
+    apiRef = apiData.refs[0].ref;
+  }
+
+  const { data, isLoading } = useQuery<APIResponse, Error>(
+    [fetchKey, apiRef, endpoint],
+    () => fetchEndpoint(apiRef!, endpoint),
+    {
+      enabled: !!apiRef,
+    }
+  );
+  return { isLoading, data };
+};
+
+const fetchAPIRef = async (): Promise<APIRefResponse> => {
+  const res = await fetch(API_BASE_URL);
+  if (res.ok) return res.json();
+
+  throw new Error('Error Fetching');
+};
 
 const fetchEndpoint = async (
   apiRef: string,
@@ -13,17 +40,4 @@ const fetchEndpoint = async (
   if (res.ok) return res.json();
 
   throw new Error('Error Fetching');
-};
-
-export const useFetch = (fetchKey: string, endpoint: string) => {
-  const { ref: apiRef } = useLatestAPI();
-
-  const { data, isLoading } = useQuery<APIResponse, Error>(
-    [fetchKey, apiRef, endpoint],
-    () => fetchEndpoint(apiRef!, endpoint),
-    {
-      enabled: !!apiRef,
-    }
-  );
-  return { isLoading, data };
 };
